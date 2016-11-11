10376a693694e7c6248d8ec3d5f134efdf30cd56
ccli8 <ccli8@nuvoton.com>
2016-11-11 15:59:58 +0800

    Change test pin to SW1 and enable de-bounce

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 // NuMaker-PFM-NUC472 : GPIO interrupt to set led on/off
 #include "mbed.h"
 
-InterruptIn button(PG_1); // D0 
-DigitalOut led(LED1);     // flashing LED1(rgbled1)
+InterruptIn button(SW1);    // Button SW1
+DigitalOut led(LED1);       // flashing LED1(rgbled1)
 
 void flip() {
     led = !led;
 }
 
 int main() {
-    button.rise(&flip); // if D0 is floating, then rgbled will be flashing
+    button.rise(&flip);
 }
