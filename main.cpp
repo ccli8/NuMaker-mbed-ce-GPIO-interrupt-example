f8ca3e92dfa48afd0eb857bd11461677ff3a0db8
shliu1 <shliu1@nuvoton.com>
2016-12-22 14:57:43 +0800

    Support M453, target is numaker_pfm_m453

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 // NuMaker-PFM-NUC472 : GPIO interrupt to set led on/off
 #include "mbed.h"
-
+#if defined(TARGET_NUMAKER_PFM_NUC472)
 InterruptIn button(SW1);    // Button SW1
 DigitalOut led(LED1);       // flashing LED1(rgbled1)
+#elif defined(TARGET_NUMAKER_PFM_M453)
+InterruptIn button(SW1);    // Button SW1
+DigitalOut led(LED1);       // flashing LED1(rgbled1)
+#endif
 
 void flip() {
     led = !led;
