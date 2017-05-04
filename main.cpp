9acbcc6a0b3076909b30827e5d2f68fb3a372cf8
ccli8 <ccli8@nuvoton.com>
2017-05-04 14:28:12 +0800

    [NUC472/M453] Support debounce setting for separate pin

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-// NuMaker-PFM-NUC472 : GPIO interrupt to set led on/off
+// GPIO interrupt to set led on/off
 #include "mbed.h"
 #if defined(TARGET_NUMAKER_PFM_NUC472)
 InterruptIn button(SW1);    // Button SW1
-DigitalOut led(LED1);       // flashing LED1(rgbled1)
+DigitalOut led(LED1);       // Flash LED1
 #elif defined(TARGET_NUMAKER_PFM_M453)
-InterruptIn button(SW1);    // Button SW1
-DigitalOut led(LED1);       // flashing LED1(rgbled1)
+InterruptIn button(SW2);    // Button SW2
+DigitalOut led(LED1);       // Flash LED1
 #endif
 
 void flip() {
